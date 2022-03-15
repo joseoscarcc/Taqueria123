c62e831f6c75d496c237b53bafa0f38c9eb7745d
joseoscarcc <joseoscarcc@gmail.com>
2022-03-14 21:26:17 -0600

    Taqueria 1 2 3
    Backend Solana Program

---
+++ programs/otro/src/lib.rs
@@ -0,0 +1,73 @@
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+use anchor_lang::prelude::*;
+
+declare_id!("Gv2KVBGbkke2hLBxm7QK32nPoVDWQhECwbw8w1nKzhdX");
+
+#[program]
+pub mod otro {
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+    // Get a reference to the account.
+    let base_account = &mut ctx.accounts.base_account;
+    // Initialize total_gifs.
+    base_account.total_gifs = 0;
+    Ok(())
+  }
+
+  // Another function woo!
+   // The function now accepts a gif_link param from the user. We also reference the user from the Context
+  pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
+    // Get a reference to the account and increment total_gifs.
+    let base_account = &mut ctx.accounts.base_account;
+    let user = &mut ctx.accounts.user;
+
+    // Build the struct.
+    let item = ItemStruct {
+      gif_link: gif_link.to_string(),
+      user_address: *user.to_account_info().key,
+    };
+
+    // Add it to the gif_list vector.
+    base_account.gif_list.push(item);
+    base_account.total_gifs += 1;
+    Ok(())
+  }
+
+}
+
+// Attach certain variables to the StartStuffOff context.
+#[derive(Accounts)]
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
+// Add the signer who calls the AddGif method to the struct so that we can save it
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+}
+
+// Create a custom struct for us to work with.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    // Attach a Vector of type ItemStruct to the account.
+    pub gif_list: Vec<ItemStruct>,
+}
